1ee98c2f8fd6ed7866b1da93f920ee1b1ad2221e
dengping_MacBook <dengpingfm@163.com>
2018-03-31 09:20:44 +0800

    2

--- src/com/adtec/ncps/BusinessException.java
+++ src/com/adtec/ncps/BusinessException.java
@@ -1,12 +1,40 @@
 package com.adtec.ncps;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import com.adtec.ncps.busi.ncp.SysPub;
+
 /**
  * 交易业务错误
  * @author dengp_w
  * @date 2018年3月28日 下午2:07:32
  */
 public class BusinessException extends RuntimeException {
-
+	
+	/**
+	 * 错误码
+	 */
+	private String errorCode ;
+	
+	/**
+	 * 错误信息
+	 */
+	private String errorMsg;
+	
+	public BusinessException(String errorCode ,String errorMsg) throws Exception {
+		this("["+errorCode+"]"+errorMsg);
+		this.errorCode = errorCode;
+		this.errorMsg = errorMsg;
+		
+//		记录详细错误堆栈信息
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		this.printStackTrace(pw);
+		SysPub.appLog("ERROR", "异常信息：[\n%s\n]",sw);
+		sw.close();
+		pw.close();
+	}
 	public BusinessException() {
 		super();
 		// TODO Auto-generated constructor stub
